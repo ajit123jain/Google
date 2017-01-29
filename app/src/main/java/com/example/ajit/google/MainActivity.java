bebbe182d65fa7ffab1b68d4cca002fb1d8bce6b
ajit1515ajit <ajitkumarjain123@gmail.com>
2017-01-29 16:07:18 +0530

    List View complete.

--- app/src/main/java/com/example/ajit/google/MainActivity.java
+++ app/src/main/java/com/example/ajit/google/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ajit.google;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] ActivityArray = {"List View","Qrscanner","Facebook login","Google Login",
+                "Github Login","Gmail","Game","Timer","Alert" , "Notification"};
+        ArrayAdapter adapter = new ArrayAdapter<>(this,
+                R.layout.activity_listview, ActivityArray);
+        ListView listView = (ListView) findViewById(R.id.Activitylist);
+        listView.setAdapter(adapter);
+
+
 
     }
 }
